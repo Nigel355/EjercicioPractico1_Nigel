5dc9c444f30dd0f7555a09b88a2f7419a7f59479
Nigel355 <ncalderon90918@ufide.ac.cr>
2025-06-25 19:17:07 -0600

    prueba comit

---
+++ EjercicioPractico_Nigel/src/main/java/com/mycompany/ejerciciopractico_nigel/EjercicioPractico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopractico_nigel;
+
+/**
+ *
+ * @author nigel
+ */
+public class EjercicioPractico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
